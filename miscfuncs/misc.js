d9d4160c07cf0a517c70b16803f2694821a2a591
Alice Guan <aliguan726@gmail.com>
2018-06-17 15:59:07 -0400

    style and build on email functionail functionality

--- miscfuncs/misc.js
+++ miscfuncs/misc.js
@@ -58,8 +58,45 @@ module.exports = {
 
     // Get the military time out from the local_time field
     processTimeMU: function (time,istrt,istop) {
-        time = time.substring(istrt,istop); 
+        time = time.substring(istrt,istop);
         return time = time.replace(":","");
+    },
+
+    convertMilTime: function (milTime) {
+        if (milTime.localeCompare("9999")===0){ //None item is populated with 999 military time
+            return "";
+        }
+        else if (milTime.localeCompare("Food")===0) {
+            return "Food"
+        }
+        if (milTime.length > 0) {
+            if (milTime.length === 3) {
+                milTime = "0" + milTime;
+            }
+
+            // fetch
+            var hours = Number(milTime.substring(0, 2));
+            var minutes = Number(milTime.substring(2, 4));
+            // var seconds = 0;
+
+            // calculate
+            var timeValue;
+
+            if (hours > 0 && hours <= 12) {
+                timeValue = "" + hours;
+            } else if (hours > 12) {
+                timeValue = "" + (hours - 12);
+            }
+            else if (hours === 0) {
+                timeValue = "12";
+            }
+
+            timeValue += (minutes < 10) ? ":0" + minutes : ":" + minutes;  // get minutes
+            timeValue += (hours >= 12) ? " P.M." : " A.M.";  // get AM/PM
+            return timeValue;
+        } else {
+            return "";
+        }
     },
 
     // Get the date from the following input
